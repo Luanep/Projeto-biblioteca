f6ebbec2cdb5247ade3f96a2a68048cb3c5c1763
Luanep <luane.sporto@gmail.com>
2021-07-06 00:26:18 -0300

    projeto

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// Se for sim, mostrar categorias disponíveis e perguntar qual prefere
+// Se não, mostrar todos os livros em ordem crescente pela quantidade de páginas
+
+const livros = require('./database')
+const readline = require('readline-sync')
+
+const entradaInicial = readline.question('Deseja buscar um livro? Digite S ou N')
+
+if (entradaInicial.toLocaleUpperCase() === 'S') {
+    console.log('Essas são as categorias disponíveis:')
+    console.log('Literatura brasileira', '/Literatura estrangeira', '/Arte', '/Poesia', '/Quadrinhos',
+     '/Tecnologia', '/Humanidades')
+
+    const entradaCategoria = readline.question('Qual categoria prefere?')
+    
+    const retorno = livros.filter(livro => livro.categoria === entradaCategoria)
+
+    console.table(retorno)
+} else {
+    const livrosOrdenados = livros.sort((a,b)=> a.paginas - b.paginas)
+    console.log('Esses são todos os livros disponiveis:')
+    console.table(livrosOrdenados)
+}
---
+++ database.js
@@ -0,0 +1,66 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Tropical Sol da Liberdade",
+        autor: "Ana Maria Machado",
+        categoria: "Literatura brasileira",
+        paginas: 376,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 2,
+        nome: "Quando os livros foram à guerra",
+        autor: "Molly Guptill Manning",
+        categoria: "Literatura estrangeira",
+        paginas: 272,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome: "Você (e todas as outras coisas que me machucam também",
+        autor: "Nalu Romano",
+        categoria: "Poesia",
+        paginas: 120,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 4,
+        nome: "Pra tudo começar na quinta feira: o enredo dos enredos",
+        autor: "Luiz Antonio Simas; Fabio Fabato",
+        categoria: "Arte",
+        paginas: 180,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: "O corpo encantado das ruas",
+        autor: "Luiz Antonio Simas",
+        categoria: "Humanidades",
+        paginas: 176,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 6,
+        nome: "Storytelling com dados",
+        autor: "Cole Nussbaumer Knaflic",
+        categoria: "Tecnologia",
+        paginas: 256,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 7,
+        nome: "Umbrella Academy Volume 2: Dallas",
+        autor: "Gerard Way",
+        categoria: "Quadrinhos",
+        paginas: 184,
+        recomenda: true,
+        leu: true
+    },
+]
+module.exports = livros
